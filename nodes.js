b6cf1eb5ad5fbb939659275662fff4b1c0c7d8ee
Meet Patel <meetaiims@yahoo.com>
2020-12-31 10:13:48 -0600

    instantiate orphan node

--- nodes.js
+++ nodes.js
@@ -1,7 +1,15 @@
 class Node {
   constructor(data) {
     this.data = data;
+    this.next = null;
   }
 }
+
+const firstNode = new Node('data for first node');
+console.log(firstNode);
+console.log(firstNode.data);
+console.log(firstNode.next);
+
+(firstNode.next) ? console.log('The node has links') : console.log('This is an orphan node');
 
 module.exports = Node;
