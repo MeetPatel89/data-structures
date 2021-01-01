3dae914ff671eb4d85775ce47ba7d219566112cc
Meet Patel <meetaiims@yahoo.com>
2020-12-31 22:26:03 -0600

    add addToTail method to linkedList class

--- LinkedList.js
+++ LinkedList.js
@@ -7,11 +7,21 @@ class LinkedList {
 
     addToHead(data) {
         const newHead = new Node(data);
-        const currentHead = this.head;
+        let currentHead = this.head;
         this.head = newHead;
         if (currentHead) {
             newHead.nextNode = currentHead;
         }
+    }
+
+    addToTail(data) {
+        const newTail = new Node(data);
+        let currentNode = this.head;
+        while(currentNode.next !== null) {
+            currentNode = currentNode.next
+        }
+        currentNode.nextNode = newTail;
+
     }
 }
 
@@ -23,4 +33,6 @@ linkedList.addToHead(" pre pre pre linked list data");
 console.log(linkedList);
 linkedList.addToHead("continuing with the experiment");
 console.log(linkedList);
+console.log('Pay attention after this');
+linkedList.addToTail("new tail data for our linked list");
 console.log(linkedList.head.next.next)
