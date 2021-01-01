cf32806f5515c2f41c3ef969db70978f9f760a8c
Meet Patel <meetaiims@yahoo.com>
2020-12-31 22:35:39 -0600

    validation to add tail to empty linked list

--- LinkedList.js
+++ LinkedList.js
@@ -1,38 +1,39 @@
-const { Node }  = require('./Node');
+const { Node } = require('./Node');
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+  constructor() {
+    this.head = null;
+  }
 
-    addToHead(data) {
-        const newHead = new Node(data);
-        let currentHead = this.head;
-        this.head = newHead;
-        if (currentHead) {
-            newHead.nextNode = currentHead;
-        }
+  addToHead(data) {
+    const newHead = new Node(data);
+    let currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      newHead.nextNode = currentHead;
     }
+  }
 
-    addToTail(data) {
-        const newTail = new Node(data);
-        let currentNode = this.head;
-        while(currentNode.next !== null) {
-            currentNode = currentNode.next
-        }
-        currentNode.nextNode = newTail;
-
+  addToTail(data) {
+    const newTail = new Node(data);
+    let currentNode = this.head;
+    if (currentNode === null) {
+      this.head = newTail;
+    } else {
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.nextNode = newTail;
     }
+  }
 }
 
 const linkedList = new LinkedList();
-console.log(linkedList)
-linkedList.addToHead("random linked list data");
-console.log(linkedList)
-linkedList.addToHead(" pre pre pre linked list data");
 console.log(linkedList);
-linkedList.addToHead("continuing with the experiment");
+linkedList.addToTail('tail data for empty linked list');
 console.log(linkedList);
-console.log('Pay attention after this');
-linkedList.addToTail("new tail data for our linked list");
+linkedList.addToHead("head data for the linked list");
+console.log(linkedList);
+linkedList.addToTail("last tail data for linked list");
+console.log(linkedList);
 console.log(linkedList.head.next.next)
