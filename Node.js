a04a711dc9769f8c77e3da42a4ee3cdab60c3f73
Meet Patel <meetaiims@yahoo.com>
2021-01-02 09:19:50 -0600

    create Node_double class for doubly linked list

--- Node.js
+++ Node.js
@@ -17,6 +17,38 @@ class Node {
   }
 }
 
+class Node_double {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.previous = null;
+  }
+
+  setNextNode(node) {
+    if (node instanceof Node_double || node === null) {
+      this.next = node;
+    } else {
+      throw new Error('Next node must be a member of the Node_double class or null')
+    }
+  }
+
+  setPreviousNode(node) {
+    if (node instanceof Node_double || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error('Next node must be a member of the Node_double class or null')
+    }
+  }
+
+  getNextNode() {
+    return this.next;
+  }
+  
+  getPreviousNode() {
+    return this.previous;
+  }
+}
+
 const firstNode = new Node('data for first node');
 
 const secondNode = new Node('data for second node');
