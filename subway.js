109731abec77630bfabb938d61312a7d76e51ccb
Meet Patel <meetaiims@yahoo.com>
2021-01-03 10:37:39 -0600

    addToTail method, subway moves from TimesSquare to PennStation to WallStreet to BrooklynBridge

--- subway.js
+++ subway.js
@@ -7,3 +7,7 @@ subway.addToHead('GrandCentral');
 subway.addToHead('CentralPark');
 subway.printList();
 console.log(subway);
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+subway.printList();
