bf45b5146b4069756f3677b63262868c7c951247
Meet Patel <meetaiims@yahoo.com>
2021-01-03 13:02:38 -0600

    add enqueue method to add nodes to queue and update size

--- Queue.js
+++ Queue.js
@@ -1,10 +1,15 @@
 const LinkedList = require('./LinkedList');
 
 class Queue {
-    constructor() {
-        this.queue = new LinkedList();
-        this.size = 0;
-    }
+  constructor() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+  }
 }
 
 const queue = new Queue();
