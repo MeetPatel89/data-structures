3a9e06f4626973b60d44d70d88407f328bb8be56
Meet Patel <meetaiims@yahoo.com>
2021-01-03 16:16:22 -0600

    add peek, pop, push methods to Stack class

--- Stack.js
+++ Stack.js
@@ -1,7 +1,31 @@
 const LinkedList = require('./LinkedList');
 
 class Stack {
-    constructor() {
-        this.stack = new LinkedList();
+  constructor() {
+    this.stack = new LinkedList();
+  }
+
+  push(value) {
+    this.stack.addToHead(value);
+  }
+
+  pop() {
+    const removedData = this.stack.removeHead();
+    return removedData;
+  }
+
+  peek() {
+    if (this.stack.head) {
+      return this.stack.head.data;
+    } else {
+      return 'The stack is empty!';
     }
+  }
 }
+
+const stack = new Stack();
+console.log(stack);
+console.log(stack.peek());
+stack.push('First stack');
+stack.push('Second stack');
+console.log(stack.pop());
