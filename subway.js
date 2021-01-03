5fb733b936ba21170f2af67b5d25a7c1e2497495
Meet Patel <meetaiims@yahoo.com>
2021-01-03 10:34:57 -0600

    subway moves from CentralPark to GrandCentral to TimesSquare

--- subway.js
+++ subway.js
@@ -1,3 +1,9 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
 const subway = new DoublyLinkedList();
+
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+subway.printList();
+console.log(subway);
