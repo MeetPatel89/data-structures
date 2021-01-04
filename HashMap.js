c7ff62cc738e931c30ba2bbe89066e50056c3aaf
Meet Patel <meetaiims@yahoo.com>
2021-01-04 10:55:51 -0600

    add hash method with hashing function to HashMap class

--- HashMap.js
+++ HashMap.js
@@ -1,12 +1,20 @@
 class HashMap {
-    constructor(size = 0) {
-        this.hashmap = new Array(size).fill(null);
+  constructor(size = 0) {
+    this.hashmap = new Array(size).fill(null);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      console.log('charcode for ' + key[i] + ' is ' + key.charCodeAt(i));
+      console.log(hashCode);
+      hashCode += key.charCodeAt(i) + hashCode;
     }
+    return hashCode;
+  }
 }
 
 const hashmap = new HashMap();
-console.log(hashmap);
-
-let fruits = new Array('apple', 'watermelon');
-console.log(fruits);
-console.log(fruits.length);
+let x = hashmap.hash("dad");
+console.log(x);
+console.log(hashmap.hash("dda"));
