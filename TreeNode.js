8fb62f0b665129d85b4df75ea7b0cf88732fa9f7
Meet Patel <meetaiims@yahoo.com>
2021-01-04 20:01:49 -0600

    add addChild method to TreeNode class

--- TreeNode.js
+++ TreeNode.js
@@ -3,9 +3,20 @@ class TreeNode {
         this.data = data;
         this.children = new Array();
     }
+
+    addChild(child) {
+        if (child instanceof TreeNode) {
+            this.children.push(child)
+        } else {
+            this.children.push(new TreeNode(child))
+        }
+    }
 }
 
 const tree = new TreeNode(1);
 console.log(tree);
-
+tree.addChild(4);
+console.log(tree);
+tree.addChild(40);
+console.log(tree);
 module.exports = TreeNode;
