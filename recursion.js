4b36653ae60d98cfe69520b2d5ebefc737e0be6a
Meet Patel <meetaiims@yahoo.com>
2021-01-06 14:54:11 -0600

    find factorial recursively using recursiveFactorial function

--- recursion.js
+++ recursion.js
@@ -1,31 +1,42 @@
-const sumFunc = inputArr => {
-    // base case
-    if (inputArr.length === 1) {
-        return inputArr[0]
-    } 
-    
-    // recursive case
-    return inputArr[0] + sumFunc(inputArr.slice(1))
-}
+const sumFunc = (inputArr) => {
+  // base case
+  if (inputArr.length === 1) {
+    return inputArr[0];
+  }
+
+  // recursive case
+  return inputArr[0] + sumFunc(inputArr.slice(1));
+};
 
 console.log(sumFunc([2, 3, 8, 9, 29]));
 
-const iterativeFactorialFor = n => {
-    let factorial = 1;
-    for (let i = 1; i <= n; i++) {
-        factorial = factorial * i;
-    }
-    return factorial;
-}
+const iterativeFactorialFor = (n) => {
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
+};
 
-const iterativeFactorialWhile = n => {
-    let factorial = 1;
-    while (n > 0) {
-        factorial = factorial * n;
-        n--;
-    }
-    return factorial;
-}
+const iterativeFactorialWhile = (n) => {
+  let factorial = 1;
+  while (n > 0) {
+    factorial = factorial * n;
+    n--;
+  }
+  return factorial;
+};
+
+const recursiveFactorial = (n) => {
+    // base case
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+
+  // recursive case
+  return n * recursiveFactorial(n - 1);
+};
 
 console.log(iterativeFactorialFor(4));
 console.log(iterativeFactorialWhile(4));
+console.log(recursiveFactorial(4));
